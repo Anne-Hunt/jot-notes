a0216c51060f2a310c510262b70c7c942441c971
Anne Hunt <acathunt@gmail.com>
2024-06-24 16:49:23 -0600

    updated appstate

--- jot-notes/client/src/AppState.js
+++ jot-notes/client/src/AppState.js
@@ -1,9 +1,42 @@
 import { reactive } from 'vue'
+import { Jot } from './models/Jot.js'
+import { Notebook } from './models/Notebook.js'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
   /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null
+  account: null,
+
+  /**@type {Jot[]} */
+  jots =[],
+
+  /**@type {Notebook[]} */
+  notebooks =[],
+
+  /**@type {Jot[]} */
+  notebookJotList =[],
+
+  notebookJotListContent = '',
+
+  notebooklistcount = 0,
+
+  notebookChangerOptionList = '',
+
+  notebookListContent = '',
+
+  activeNotebook = null,
+
+  activeJot = null,
+
+  intervalID = null,
+
+  noteCount = 0,
+
+  characterCount = 0,
+
+  wordCount = 0
+
+}
 })
---
+++ jot-notes/client/src/models/Jot.js
@@ -0,0 +1,17 @@
+import { generateId } from "../utils/GenerateId.js"
+
+export class Jot {
+
+    //** @param {{id, name, body, tags, color, notebook}} data */
+
+    constructor(data) {
+        this.id = generateId()
+        this.name = data.name
+        this.body = data.body || ''
+        this.tags = data.tags
+        this.color = data.color
+        this.createdAt = data.createdAt == undefined ? new Date() : new Date(data.createdAt)
+        this.editedAt = data.editedAt == undefined ? new Date() : new Date(data.editedAt)
+        this.notebook = data.notebook || ''
+    }
+}
