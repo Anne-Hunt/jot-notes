8e8f96ebf35443aff31159bc16b67b757a69ceee
Anne Hunt <acathunt@gmail.com>
2024-06-24 17:46:03 -0600

    created modal and offcanvas

---
+++ jot-notes/client/src/components/OffCanvas.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+
+<template>
+<div class="offcanvas offcanvas-start show" tabindex="-1" id="offcanvas" aria-labelledby="ocLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="ocLabel">Create</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <!--INSERT CONTENT-->
+  </div>
+</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
