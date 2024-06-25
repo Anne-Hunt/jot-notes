fcccb9a77f6b85734555a7e6402991229ac55c7e
Anne Hunt <acathunt@gmail.com>
2024-06-25 13:34:51 -0600

    added color list

--- jot-notes/client/src/App.vue
+++ jot-notes/client/src/App.vue
@@ -22,6 +22,27 @@ import Navbar from './components/Navbar.vue'
 
 :root {
   --main-height: calc(100vh - 32px - 64px);
+  --c-navy: #001f3f;
+--c-blue: #0074D9;
+--c-aqua: #7FDBFF;
+--c-teal: #39CCCC;
+--c-purple: #B10DC9;
+--c-indigo: #4b0082;
+--c-fuchsia: #F012BE;
+--c-maroon: #85144b;
+--c-red: #FF4136;
+--c-orange: #FF851B;
+--c-yellow: #FFDC00;
+--c-olive: #3D9970;
+--c-green: #2ECC40;
+--c-lime: #01FF70;
+--c-black: #111111;
+--c-grey: #AAAAAA;
+--c-silver: #DDDDDD;
+--c-white: #FFFFFF;
+--c-primary: #8254ad;
+--c-secondary: #603e80;
+--c-tertiary: #ad68ed;
 }
 
 
