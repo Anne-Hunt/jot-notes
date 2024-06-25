c2ac8748f60792fc5e2f3e8ed22e6d6a26a2e841
Anne Hunt <acathunt@gmail.com>
2024-06-24 18:03:14 -0600

    created form components

---
+++ jot-notes/client/src/components/JotForm.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+
+<template>
+<form action=""></form>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ jot-notes/client/src/components/NotebookForm.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+
+<template>
+<form action="">
+    
+</form>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
