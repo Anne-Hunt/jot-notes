7509a28923803c3b9e5fbd4cedcbcedd1eb5f21b
Anne Hunt <acathunt@gmail.com>
2024-08-01 19:00:10 -0600

    aligning jot components properly on each page

--- jot-notes/client/src/components/JotListItem.vue
+++ jot-notes/client/src/components/JotListItem.vue
@@ -8,16 +8,19 @@ defineProps({jot: Jot})
 
 
 <template>
-<div class="rounded mb-1 shadow size" :style="{backgroundColor: `${jot.color}`}">
+<div class="rounded mb-1 shadow size" >
     <RouterLink :to="{name: 'Jot', params: {jotId: `${jot.id}`}}">
         <div class="row h-100 align-items-center">
-            <div class="col-1"></div>
+            <div class="col-1 rounded bg-dark p-0 m-0">
+                <i class="mdi mdi-book fs-1" :style="{color: `${jot.color}`}"></i>
+            </div>
             <div class="col-11">
-
-                <h3 class="text-light fontfix text-truncate">
-                    <i v-if="jot.private == true" class="mdi mdi-lock fontfix"></i>
+                <h3 class="text-dark">
                     {{ jot.name }}
                 </h3>
+                <div class="text-truncate book">
+                    <i v-if="jot.private == true" class="mdi mdi-lock text-dark bottom-right fontfix"></i>
+                </div>
             </div>
         </div>
     </RouterLink>
@@ -30,7 +33,14 @@ defineProps({jot: Jot})
 .fontfix{
   text-shadow: 1px 1px 4px black;
 }
-
+.book{
+    position: relative;
+}
+.bottom-right {
+  position: absolute;
+  bottom: 8px;
+  right: 16px;
+}
 .size{
     height: 10dvh;
 }
